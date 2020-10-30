1d30901b07a10d2c3c5ff3382491569c4a399a03
ankur.rathi <ankur.rathi@webiantechnologies.com>
2020-10-30 10:45:05 +0530

    Interest calculation done based on simple interest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,6 +27,9 @@ export class AppComponent implements OnInit {
     this.createForm();
   }
 
+  /**
+   * Bond Yield Input Form
+   */
   createForm(): void {
     this.emiForm = new FormGroup({
       amount: new FormControl(''),
@@ -35,24 +38,50 @@ export class AppComponent implements OnInit {
     });
   }
 
+  /**
+   * On Payment Frequency Changes
+   * @param evt
+   */
   onYearChange(evt): void {
     const year = this.emiForm.value.year;
     this.rateOfInterest = this.paymentFrequency.filter(element => element.year == year);
     this.emiForm.get('roi').setValue(this.rateOfInterest[0].roi);
   }
 
+  /**
+   * On Calculate Button Click
+   */
   onCalculate(): void {
     const data = this.emiForm.value;
 
-    this.totalInterest = this.compoundInterest(data.amount, (data.roi / 100), data.year).toFixed(2);
+    this.totalInterest = this.simpleInterest(data.amount, (data.roi / 100), data.year).toFixed(2);
     this.initialBalance = data.amount;
     this.totalAmount = parseInt(this.initialBalance, 10) + parseFloat(this.totalInterest);
     this.showProjection = true;
   }
 
+  /**
+   * Compount Interest Calculation
+   * @param principal
+   * @param rate
+   * @param time
+   * @param frequency
+   */
   compoundInterest(principal, rate, time, frequency = 1): any {
     const amount = principal * (Math.pow((1 + (rate / frequency)), (frequency * time)));
     const interest = amount - principal;
+    return interest;
+  }
+
+  /**
+   * Simple Interest Calculation
+   * @param principal
+   * @param rate
+   * @param time
+   */
+  simpleInterest(principal, rate, time): any {
+    const amount = principal * rate * time;
+    const interest = amount;
     return interest;
   }
 }
